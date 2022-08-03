8aa4b8a9afad189327bc582b2b85be57a3c141c5
brooklyneti2012 <ac106494@avcol.school.nz>
2022-08-03 12:29:49 +1200

    yes

--- AvUni/Areas/Identity/Data/AvUniContext.cs
+++ AvUni/Areas/Identity/Data/AvUniContext.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using AVC.Models;
+using System.Linq;
+using AvUni.Models;
 
 namespace AvUni.Data;
 
@@ -20,6 +22,65 @@ public class AvUniContext : IdentityDbContext<IdentityUser>
         // Add your customizations after calling base.OnModelCreating(builder);
     }
 
+    public static class DbInitializer
+    {
+        public static void Initialize(AvUniContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if(context.Product.Any())
+            {
+                return;
+            }
+
+            var products = new Product[]
+            {
+                new Product{Title="Jacket - Sizes 12 - 14 years", Category="Optional", Price="$96.00"},
+                new Product{Title="Jacket - Sizes xxs - xs", Category="Optional", Price="$110.00"},
+                new Product{Title="Jacket - Sizes Small - 5Xl", Category="Optional", Price="$135.00"},
+                new Product{Title="Scarf", Category="Optional", Price="$28.50"},
+                new Product{Title="Cap", Category="Optional", Price="$23.00"},
+                new Product{Title="Long black socks with double white strip", Category="Optional", Price="$18.00"},
+                new Product{Title="Tie", Category="Optional", Price="$29.00"},
+                new Product{Title="Blazer", Category="Optional", Price="$220.00"},
+                new Product{Title="", Category="Junior", Price=""},
+                new Product{Title="", Category="Junior", Price=""},
+                new Product{Title="", Category="Junior", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+                new Product{Title="", Category="", Price=""},
+            };
+        }
+    }
     public DbSet<AVC.Models.Order>? Order { get; set; }
 
     public DbSet<AVC.Models.Product>? Product { get; set; }
